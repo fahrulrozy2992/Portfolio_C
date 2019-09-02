ff0baa10c28b3bc361f9e84de01e98317213c787
fahurlrozy <fahrulrozy2992@gmail.com>
2019-09-02 09:52:46 +0700

    membuat Weapon

---
+++ Portfolio_C/Assets/Scrip/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Car_Shooter
+{
+
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Item/Weapon", order = 1)]
+
+    public class Weapon : ScriptableObject
+    {
+        public int amount;
+        public int damage;
+    }
+}
