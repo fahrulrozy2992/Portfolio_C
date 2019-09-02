87751823d1a7df7d222681dd8e8fe36c673f204e
fahurlrozy <fahrulrozy2992@gmail.com>
2019-09-02 09:34:03 +0700

    Scrip car

---
+++ Portfolio_C/Assets/Scrip/Car.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Car_Shooter
+{
+    public class Car : MonoBehaviour
+    {
+
+        public WheelJoint2D bandepan;
+        public WheelJoint2D banbelakang;
+        public JointMotor2D motor;
+        public float maju = 600f;
+        public float rem = 300f;
+        public float mundur = 100f;
+        // Use this for initialization
+        void Start()
+        {
+            motor.maxMotorTorque = 10000f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            move();
+        }
+
+        void move()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (motor.motorSpeed > 0)
+                    motor.motorSpeed = 0;
+                motor.motorSpeed -= maju * Time.deltaTime;
+                bandepan.motor = motor;
+                banbelakang.motor = motor;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                if (motor.motorSpeed < 0)
+                    motor.motorSpeed = 0;
+                motor.motorSpeed += mundur * Time.deltaTime;
+                bandepan.motor = motor;
+                banbelakang.motor = motor;
+            }
+            else
+            {
+                motor.motorSpeed += rem * Time.deltaTime;
+                motor.motorSpeed = Mathf.Clamp(motor.motorSpeed, -10000f, 0);
+                bandepan.motor = motor;
+                banbelakang.motor = motor;
+            }
+        }
+    }
+}
